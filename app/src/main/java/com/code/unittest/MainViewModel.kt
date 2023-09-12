beced17c188e6a6163537a2719bfca3270b196d3
lalubramadhan <lalubagoes2@gmail.com>
2023-09-12 19:44:34 +0700

    init

---
+++ app/src/main/java/com/code/unittest/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.code.unittest
+
+class MainViewModel(val cuboidViewModel: CuboidModel) {
+    fun getCircumferences() = cuboidViewModel.getCircumferences()
+
+    fun getSurfaceArea() = cuboidViewModel.getSurfaceArea()
+
+    fun getVolume() = cuboidViewModel.getVolume()
+
+    fun save(w:Double, l:Double, h: Double){
+        cuboidViewModel.save(w,l,h)
+    }
+}
